bd9f27708ad0330bde69722557ba4840f6b776fa
ArcticLampyrid <ArcticLampyrid@outlook.com>
2022-09-26 15:52:01 +0800

    fix: follow system layout for config path

--- CMakeCaller/Kits/CMakeKitsController.cs
+++ CMakeCaller/Kits/CMakeKitsController.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using BlackFox.VsWhere;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
-using System.Text;
-using System.Threading.Tasks;
-using BlackFox.VsWhere;
-using System.Text.Json;
+using System.Runtime.InteropServices;
 using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace QIQI.CMakeCaller.Kits
 {
@@ -15,20 +16,38 @@ namespace QIQI.CMakeCaller.Kits
         public static string CacheFileV1 { get; }
         static CMakeKitsController()
         {
-            string localAppData;
+            string configPath;
             try
             {
-                localAppData = Environment.GetFolderPath(
-                    Environment.SpecialFolder.LocalApplicationData, 
-                    Environment.SpecialFolderOption.Create);
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    configPath = Environment.GetEnvironmentVariable("XDG_CONFIG_HOME")
+                        ?? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".config");
+                    configPath = Path.Combine(configPath, $"{nameof(QIQI)}.{nameof(CMakeCaller)}");
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    configPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Library/Application Support");
+                    configPath = Path.Combine(configPath, $"{nameof(QIQI)}.{nameof(CMakeCaller)}");
+                }
+                else
+                {
+                    // Windows or others
+                    configPath = Environment.GetFolderPath(
+                        Environment.SpecialFolder.LocalApplicationData,
+                        Environment.SpecialFolderOption.Create);
+                    configPath = Path.Combine(configPath, $"{nameof(QIQI)}.{nameof(CMakeCaller)}", "Config");
+                }
+                Directory.CreateDirectory(configPath);
             }
             catch (Exception)
             {
-                localAppData = Path.GetTempPath(); //fallback
+                // fallback
+                Debug.WriteLine("Fallback: use temporary folder to store kits.");
+                configPath = Path.Combine(Path.GetTempPath(), $"{nameof(QIQI)}.{nameof(CMakeCaller)}", "Config");
+                Directory.CreateDirectory(configPath);
             }
-            var cacheFolder = Path.Combine(localAppData, ".QIQI.CMakeCaller");
-            Directory.CreateDirectory(cacheFolder);
-            CacheFileV1 = Path.Combine(cacheFolder, "kits-cache-v1.json");
+            CacheFileV1 = Path.Combine(configPath, "kits-cache-v1.json");
         }
         public static ReadOnlyCollection<CMakeKitInfo> GetKits()
         {
